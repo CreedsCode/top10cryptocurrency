8d20cad35dc9cfa2a847997444c5d3de7e65fdf9
CreedsCode <hoffmann.dercio@hotmail.com>
2018-08-29 06:34:05 +0200

    First init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div  id="app">
+    <h1>Top 10 Crypto Currency</h1>
+    <div id="crypto-container" v-for="(value,key) in cryptos">
+      <span class="left">{{ key }}</span>
+      <span class="right">{{ value.EUR }}€</span>
+    </div>
+    <p>Updated "28 August 2018"</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data: () => ({
+    cryptos: [],
+    errors: []
+  }),
+  created() {
+    axios
+      .get(
+        "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,EOS,BCH,XRP,ETC,DASH,NEO,IOTA&tsyms=EUR"
+      )
+      .then(response => {
+        this.cryptos = response.data;
+        console.log(response);
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
   }
-}
+};
 </script>
 
 <style>
+p {
+  text-align: center;
+}
+h1 {
+  text-align: center;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background: #f1f1f1;
+}
+
+div#crypto-container {
+  background: white;
+  width: 70%;
+  margin: 0 auto 4px auto;
+  padding: 1em;
+  box-shadow: 1px 1px 0 lightgrey;
+}
+
+span.left {
+  font-weight: bold;
+}
+
+span.right {
+  float: right;
 }
 </style>
